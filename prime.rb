b8f6b0b88ddccc9c86e3e8278a8d6624aa679bfd
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-14 00:04:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,12 @@
-def prime(number)
-  if number == 0 or number == 1 
+def prime?(value)
+  if value <= 1 || value == 0 || value == 1
     return false
-  end
-  
-  i = 2 
-  limit = number / i
-  while i < limit
-    if number % i == 0
+  elsif
+    (2..value - 1).each do |i|
+      if value % i == 0
         return false
+      end
     end
-    i += 1 
-    limit = number / i 
   end
-  return true
+  true 
 end
